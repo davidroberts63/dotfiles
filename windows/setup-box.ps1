6bf1ce376b477eb38612cd722ab9782a45a8da91
David Roberts <david.roberts63@gmail.com>
2017-10-18 18:12:29 -0500

    Include Chrome and trust chocolatey

--- windows/setup-box.ps1
+++ windows/setup-box.ps1
@@ -1,11 +1,13 @@
 Write-Host "Getting ChocolateyGet..."
 Install-PackageProvider ChocolateyGet
 Import-PackageProvider ChocolateyGet
+Register-PackageSource -Name Chocolatey -Provider ChocolateyGet -Trusted -Location https://www.chocolatey.org
 
 Write-Host "Installing packages..."
 @(
     "poshgit",
     "putty",
     "firefox",
-    "nuget.commandline"
+    "nuget.commandline",
+    "googlechrome"
 ) | %{ Install-Package $_ -Provider ChocolateyGet -Verbose }
