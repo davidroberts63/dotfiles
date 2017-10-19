e194447c52f4307b3a1de8c1585cb20aa9d28629
David Roberts <david.roberts63@gmail.com>
2017-10-18 21:29:47 -0500

    Check for installed packages and include extension

--- windows/setup-box.ps1
+++ windows/setup-box.ps1
@@ -1,18 +1,30 @@
 # Copy paste the line below in administrator powershell prompt
 # Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/davidroberts63/dotfiles/master/windows/setup-box.ps1'))
 
-if(-not (get-command FooBar -ErrorAction SilentlyContinue)) {
+if(-not (get-command choco.exe -ErrorAction SilentlyContinue)) {
   Write-Host 'Installing Chocolatey'
   Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-Write-Host "Installing packages..."
+Write-Host "=== Installing packages..."
 @(
     "poshgit",
     "putty",
     "firefox",
     "nuget.commandline",
-    "googlechrome"
+    "googlechrome",
+    "visualstudiocode"
 ) | %{ 
-  & cinst.exe $_ -y
+  if(-not (& choco info $_ --localonly --limitoutput)) {
+    & cinst.exe $_ -y
+  } else {
+    Write-Host $_ already installed.
+  }
+}
+
+Write-Host '=== Installing VS Code Extensions...'
+@(
+    'humao.rest-client'
+) | %{
+  & code.cmd --install-extension $_
 }
